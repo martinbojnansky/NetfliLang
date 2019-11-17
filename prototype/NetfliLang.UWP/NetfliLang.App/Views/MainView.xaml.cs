bc3cea106c9d71ced3950f8a6ec8667b58ae8496
Martin Bojnanský <martin.bojnansky@outlook.com>
2019-11-17 22:02:42 +0100

    Implements notifications between webview and eval functions

--- prototype/NetfliLang.UWP/NetfliLang.App/Views/MainView.xaml.cs
+++ prototype/NetfliLang.UWP/NetfliLang.App/Views/MainView.xaml.cs
@@ -1,23 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using NetfliLang.Core.Navigation;
 using NetfliLang.App.ViewModels;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.Threading.Tasks;
 using System.Diagnostics;
-using Windows.System;
 using Windows.UI.ViewManagement;
+using NetfliLang.Messaging;
 
 namespace NetfliLang.App.Views
 {
@@ -25,128 +15,31 @@ namespace NetfliLang.App.Views
     {
         public MainViewModel ViewModel { get; set; } = ((App)Application.Current).MvvmLocator.ResolveViewModel<MainViewModel>();
 
-        private DispatcherTimer dispatcherTimer;
-        private List<string> titles = new List<string>();
-        private int titleIndex = 0;
-
         public MainView()
         {
-            this.InitializeComponent();
+            InitializeComponent();
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        private void NetflixWebview_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
         {
-            base.OnNavigatedTo(e);
-
-            dispatcherTimer = new DispatcherTimer();
-            dispatcherTimer.Tick += DispatcherTimer_Tick;
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 2);
-            dispatcherTimer.Start();
+            NetflixWebview.AddWebAllowedObject(nameof(NetfliLang), new WebViewMessenger());
         }
 
-        protected override void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            dispatcherTimer.Stop();
-            base.OnNavigatedFrom(e);
-        }
-
-        private async void DispatcherTimer_Tick(object sender, object e)
-        {
-            await LogTitle();
-        }
-
-        private async Task LogTitle()
+        private async void NetflixWebview_DOMContentLoaded(WebView sender, WebViewDOMContentLoadedEventArgs args)
         {
             try
             {
-                var title = await NetflixWebview.InvokeScriptAsync("document.querySelector('.player-timedtext-text-container').innerText");
-                title = title?.Replace("\n", "\\n");
-
-                if (!string.IsNullOrWhiteSpace(title) && !title.Contains("Exception from HRESULT:"))
-                {
-                    if (titles.Count > 0)
-                    {
-                        if (!titles[titles.Count - 1].Equals(title))
-                        {
-                            titles.Add(title);
-
-                            if (titles.Count > 10)
-                            {
-                                titles.RemoveAt(0);
-                            }
-                        }
-                    } else {
-                        titles.Add(title);
-
-                        if (titles.Count > 10)
-                        {
-                            titles.RemoveAt(0);
-                        }
-                    }
-                }
+                await NetflixWebview.InvokeScriptAsync($"{nameof(NetfliLang)}.sendNotification('xxx');");
             }
             catch { }
         }
 
-        private async Task ShowTitle()
+        private void ScriptNotify(object sender, NotifyEventArgs e)
         {
-            try
-            {
-                var title = titles[titleIndex];
-                await TranslatorWebview.InvokeScriptAsync($"document.querySelector('#source').value = '{title}'");
-                ViewModel.ShowTranslation = true;
-            }
-            catch { }
+            Debug.WriteLine($"script: {e?.Value}");
         }
 
-        private async void ToggleButton_Checked(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                await NetflixWebview.InvokeScriptAsync("document.querySelector('video').pause()");
-                await LogTitle();
-                titleIndex = titles.Count - 1;
-
-                if (titles.Count > 0)
-                {
-                    await ShowTitle();
-                }
-            }
-            catch { }
-        }
-
-        private async void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                ViewModel.ShowTranslation = false;
-                await NetflixWebview.InvokeScriptAsync("document.querySelector('video').play()");
-            }
-            catch { }
-        }
-
-        private async void ToggleButton_RightTapped(object sender, RightTappedRoutedEventArgs e)
-        {
-            try
-            {
-                await NetflixWebview.InvokeScriptAsync("document.querySelector('video').pause()");
-                await LogTitle();
-
-                if (!ViewModel.ShowTranslation)
-                {
-                    titleIndex = titles.Count - 1;
-                }
-
-                titleIndex--;
-                if (titleIndex >= 0)
-                {
-                    await ShowTitle();
-                }
-            }
-            catch { }
-        }
-
-        private void NetflixWebview_ContainsFullScreenElementChanged(WebView sender, object args)
+        private void ContainsFullScreenElementChanged(WebView sender, object args)
         {
             if (sender.ContainsFullScreenElement)
             {
@@ -156,39 +49,6 @@ namespace NetfliLang.App.Views
                 ApplicationView.GetForCurrentView().ExitFullScreenMode();
             }
         }
-
-        //private async void NetflixWebview_ScriptNotify(object sender, NotifyEventArgs e)
-        //{
-        //    try
-        //    {
-        //        switch (e.Value)
-        //        {
-        //            case "pause":
-        //                await LogTitle();
-        //                titleIndex = titles.Count - 1;
-
-        //                if (titles.Count > 0)
-        //                {
-        //                    await ShowTitle();
-        //                }
-        //                break;
-        //            case "play":
-        //                ViewModel.ShowTranslation = false;
-        //                break;
-        //        }
-        //    }
-        //    catch { }
-        //}
-
-        //private async void NetflixWebview_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
-        //{
-        //    try
-        //    {
-        //        // https://www.suchan.cz/2016/01/hacking-uwp-webview-part-2-bypassing-window-external-notify-whitelist/
-        //        await NetflixWebview.InvokeScriptAsync("function getVideo() { return document.querySelector('video'); } function start() { let prevVideo = document.querySelector('video'); setInterval(() => { const nextVideo = getVideo(); if (prevVideo !== nextVideo) { prevVideo = nextVideo; if (nextVideo) { nextVideo.addEventListener('play', () => { console.log('play'); window.external.notify('play'); }); nextVideo.addEventListener('pause', () => { console.log('pause'); window.external.notify('pause'); }); } } }, 500); } start(); window.external.notify('pause');");
-        //    }
-        //    catch { }
-        //}
     }
 
     public static class Extensions
---
+++ prototype/NetfliLang.UWP/NetfliLang.Messaging/WebViewMessenger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+
+namespace NetfliLang.Messaging
+{
+    public delegate void SendNotificationHandler(string notification);
+
+    [AllowForWeb]
+    public sealed class WebViewMessenger
+    {
+        public event SendNotificationHandler SendNotificationEvent;
+
+        public void sendNotification(string notification)
+        {
+            Debug.WriteLine($"notification: {notification}");
+        }
+    }
+}
