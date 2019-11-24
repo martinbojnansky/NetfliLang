b86c80d7edce79b7a2de4260a5865c89bbf2e99b
Martin Bojnanský <martin.bojnansky@outlook.com>
2019-11-24 01:04:05 +0100

    Adds mutation obsererver and parses subtitles into HashList structure.

--- prototype/NetfliLang.UWP/NetfliLang.App/Scripts/main.js
+++ prototype/NetfliLang.UWP/NetfliLang.App/Scripts/main.js
@@ -1,26 +1,18 @@
 ﻿let nlSubtitles;
-let nlTimer;
 let nlStyle;
-let nlVideo;
 let nlSpeed = 0.7;
-
-let nlCurrentIndex;
-let nlDisplayedIndex;
-let nlTranslatedIndex;
 
 let xhrOpen = window.XMLHttpRequest.prototype.open;
 window.XMLHttpRequest.prototype.open = function (method, url, async, user, password) {
     this.addEventListener('load', function () {
         try {
-            // if (!window.location.href.includes('netflix.com/watch')) return;
+            if (!window.location.href.includes('netflix.com/watch')) return;
 
             const parser = new DOMParser();
             const ttmlDoc = parser.parseFromString(this.responseText, 'text/xml');
             nlSubtitles = parseSubtitles(ttmlDoc);
-
-            if (nlTimer) return;
+            console.log(nlSubtitles);
             createSubtitlesStyle();
-            nlTimer = setInterval(onTick, 500);
         }
         catch (e) {
             // Most likely this is not ttml document.
@@ -30,9 +22,24 @@ window.XMLHttpRequest.prototype.open = function (method, url, async, user, passw
     return xhrOpen.apply(this, arguments);
 };
 
-function onTick() {
+let insertedNodes;
+let observer = new WebKitMutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+        insertedNodes = [];
+        for (var i = 0; i < mutation.addedNodes.length; i++)
+            insertedNodes.push(mutation.addedNodes[i]);
+    });
+    
+    // TODO: If video, apply speed "video"
     setSpeed();
-}
+    // TODO: If subtitles, show translation and translate next ".player-timedtext"
+});
+observer.observe(document, {
+    childList: true,
+    subtree: true
+});
+
+// https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
 
 function getVideo() {
     return document.querySelector('video');
@@ -41,6 +48,8 @@ function getVideo() {
 function setSpeed() {
     trySafe(() => getVideo().playbackRate = nlSpeed);
 }
+
+function updateTranslation()
 
 function createSubtitlesStyle() {
     if (!nlStyle) {
@@ -57,8 +66,8 @@ function createSubtitlesStyle() {
             }
 
             .player-timedtext span:before {
+                content: '...';                
                 display: block;
-                content: '...';
                 color: white;
                 font-size: 25px;
                 line-height: normal;
@@ -79,20 +88,37 @@ function createSubtitlesStyle() {
 
 function parseSubtitles(ttmlDoc) {
     const tickRate = Number(ttmlDoc.querySelector('tt').getAttribute('ttp:tickRate'));
-    return Array.from(ttmlDoc.querySelectorAll('p')).map(p => {
-        return {
-            start: Number(p.getAttribute('begin').replace('t', '')) / tickRate,
-            end: Number(p.getAttribute('end').replace('t', '')) / tickRate,
-            values: Array.from(p.querySelectorAll('span')).map(s => s.textContent),
-            translations: null
-        };
+    const subtitles = {};
+    ttmlDoc.querySelectorAll('p').forEach((element, index, elements) => {
+        const key = element.textContent;
+        const nextKey = index < elements.length - 1 ? elements[index + 1].textContent : null;
+        const occurence = parseOccurence(element, tickRate, nextKey);
+        if (!subtitles[key]) {
+            const lines = Array.from(element.querySelectorAll('span')).map(s => s.textContent);
+            subtitles[key] = {
+                occurences: [occurence],
+                lines: lines,
+                translations: null
+            };
+        } else {
+            subtitles[key].occurences.push(occurence);
+        }
     });
+    return subtitles;
+}
+
+function parseOccurence(element, tickRate, nextKey) {
+    return {
+        start: Number(element.getAttribute('begin').replace('t', '')) / tickRate,
+        end: Number(element.getAttribute('end').replace('t', '')) / tickRate,
+        next: nextKey
+    };
 }
 
 function trySafe(fce) {
     try {
         fce();
     } catch (e) {
-        // 
+        return null;
     }
 }
