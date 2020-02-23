289ff475c5732dcbc03d29f9eb53d9f0b4d6c7ac
Martin Bojnanský <martin.bojnansky@outlook.com>
2020-02-24 00:20:39 +0100

    Adds speed configuration.

--- NetfliLang.UWP/NetfliLang.App/Scripts/netflix.ts
+++ NetfliLang.UWP/NetfliLang.App/Scripts/netflix.ts
@@ -1,3 +1,6 @@
 import { NetflixService } from "./services/netflix.service";
+import { sendNotification } from "./helpers/notifications";
 
 export const netflix = new NetflixService();
+
+sendNotification('ready', null);
