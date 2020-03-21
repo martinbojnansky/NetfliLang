adfe5021b1b32fe9b928a718872b9990cc081740
Martin Bojnanský <martin.bojnansky@outlook.com>
2020-03-21 15:37:26 +0100

    Fixes app packages creation.

--- NetfliLang.UWP/NetfliLang.App/App.xaml.cs
+++ NetfliLang.UWP/NetfliLang.App/App.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using NetfliLang.Core.IoC;
 using NetfliLang.App.IoC;
 using NetfliLang.App.Views;
