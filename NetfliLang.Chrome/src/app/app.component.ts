62aa004fa1a413cf05e7ebe41d22c90ea1f24031
Martin Bojnanský <martin.bojnansky@outlook.com>
2020-05-23 00:31:02 +0200

    Fixes communication by removing async callback

--- NetfliLang.Chrome/src/app/app.component.ts
+++ NetfliLang.Chrome/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,6 @@ export class AppComponent {
   constructor() {}
 
   chromeAction() {
-    console.log('xxx');
-    chrome.runtime.sendMessage({ action: 'set lang' });
+    console.log('popup action');
   }
 }
--- NetfliLang.Chrome/src/scripts/background.ts
+++ NetfliLang.Chrome/src/scripts/background.ts
@@ -1,19 +1,20 @@
-import { onMessage, getOrCreateTab } from './shared/extension-helpers';
+import { getOrCreateTab, onMessage } from './shared/extension-helpers';
 import { Action } from 'src/shared/actions';
 import { Constants } from 'src/shared/constants';
 
 chrome.runtime.onInstalled.addListener(function () {
-  onMessage(async (m) => {
+  onMessage((m) => {
     // Translator messages
     if ([Action.translate].includes(m.action)) {
-      console.log('creating tab');
-      const translatorTab = await getOrCreateTab(Constants.translatorUrl);
-      chrome.tabs.sendMessage(translatorTab.id, m);
+      getOrCreateTab(Constants.translatorUrl).then((tab) => {
+        chrome.tabs.sendMessage(tab.id, m);
+      });
     }
     // Netflix messages
     else if ([Action.translated].includes(m.action)) {
-      const netflixTab = await getOrCreateTab(Constants.netflixUrl);
-      chrome.tabs.sendMessage(netflixTab.id, m);
+      getOrCreateTab(Constants.netflixUrl).then((tab) => {
+        chrome.tabs.sendMessage(tab.id, m);
+      });
     }
     // Default
     else {
